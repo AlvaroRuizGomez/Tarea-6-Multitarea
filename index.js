e420ca58ca029135bda2bcab9c179ada04ff221a
@lvaroRuiz <alvarorrgomez@gmail.com>
2021-02-23 19:18:42 +0100

    Menu principal llama a las diferente paginas

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Desde menu principal')
+const convert = document.getElementById("convertidor");
+const autonomo = document.getElementById("autonomo");
+const zombie = document.getElementById("zombie");
+const calculator = document.getElementById("calculadora");
+
+
+convert.addEventListener("click", () =>
+{
+    window.location.href = "../_divisas/divisas.html";
+});
+
+autonomo.addEventListener("click", () =>
+{
+    window.location.href = "../_autonomo/autonomo.html";
+});
+
+zombie.addEventListener("click", () =>
+{
+    window.location.href = "../_zombie/zombie.html";
+});
+
+calculator.addEventListener("click", () =>
+{
+    window.location.href = "../_calculadora/calculadora.html";
+});
